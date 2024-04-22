05eed8e6f1043b563c51c885650171e756e59009
Keegan <keeganjoubert@protonmail.com>
2024-04-22 09:53:34 -0700

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,12 @@
 #!/usr/bin/pup
 # Install Flask from pip3
 
-package { 'python3-pip':
-  ensure => installed,
+package { 'flask':
+   ensure   => '2.1.0',
+   provider => 'pip3',
 }
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  unless  => '/usr/local/bin/flask --version | grep -q "2.1.0"',
-  require => Package['python3-pip'],
+#Using Puppet, Install Werkzeug from pip3
+package { 'Werkzeug':
+   ensure   => '2.1.1',
+   provider => 'pip3',
 }
